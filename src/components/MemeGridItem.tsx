b24db07c92347094cf61162a308f76e64c42578f
Waleed <waleed.nizamaniwn@gmail.com>
2024-09-13 17:54:24 +0200

    added scrolling ability so that selected item is auto scrolled to

--- src/components/MemeGridItem.tsx
+++ src/components/MemeGridItem.tsx
@@ -2,6 +2,7 @@ import { ModelEmoji } from "@/classes/ModelEmoji";
 import { EnumSearchType } from "@/enums";
 import { useMemeStore } from "@/hooks/useMemes";
 import { showErrorToast, showSuccessToast } from "@/utils/utils";
+import { useEffect, useRef } from "react";
 
 interface GridItemProps {
     item: ModelEmoji;
@@ -11,14 +12,22 @@ interface GridItemProps {
 
 const GridItem: React.FC<GridItemProps> = ({ item, index }: GridItemProps) => {
     const [currentSelectedGifIndex, currentSearchItems] = useMemeStore((state) => [state.currentSelectedGifIndex, state.currentSearchItems]);
+    const itemRef = useRef<HTMLDivElement | null>(null);
 
     const safeCustomToString = (item: ModelEmoji) => {
         return item.customToString ? item.customToString() : `${item.shortName} - ${item.emoji} - ${item.searchType} - ${item.memeUrl}`;
     }
-
-    // console.log(item);
     
     const isSelectedItem: boolean = index === currentSelectedGifIndex;
+    useEffect(() => {
+        if (isSelectedItem && itemRef.current) {
+            itemRef.current.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center',
+                inline: 'center'
+            });
+        }
+    }, [isSelectedItem]);
     console.log("Is selected item: "+ isSelectedItem.toString() + " index: " + index.toString() + " currentSelectedGifIndex: " + currentSelectedGifIndex.toString());
     // const isSelectedItem: boolean = currentSelectedGif !== null && currentSelectedGif instanceof  ModelEmoji && (item.customToString() === currentSelectedGif.customToString());
 
@@ -68,7 +77,7 @@ const GridItem: React.FC<GridItemProps> = ({ item, index }: GridItemProps) => {
     
     if (item.searchType === EnumSearchType.ascii || item.searchType === EnumSearchType.emojis) {
         return (
-            <div onClick={onClick} key={safeCustomToString(item)} className={`w-44 h-44 text-white rounded-lg bg-gray-3 flex flex-col justify-center items-center m-3 ${isSelectedItem ? "border-solid border-2 border-selected" : ""} hover:border-solid hover:border-2 hover:border-selected cursor-pointer`}>
+            <div ref={itemRef} onClick={onClick} key={safeCustomToString(item)} className={`w-44 h-44 text-white rounded-lg bg-gray-3 flex flex-col justify-center items-center m-3 ${isSelectedItem ? "border-solid border-2 border-selected" : ""} hover:border-solid hover:border-2 hover:border-selected cursor-pointer`}>
 
                 <div className={`mb-4 ${item.searchType === EnumSearchType.emojis ? "emoji-mart-emoji" : ""}`}>
                     {item.emoji}
@@ -82,7 +91,7 @@ const GridItem: React.FC<GridItemProps> = ({ item, index }: GridItemProps) => {
         );
     } else if (item.searchType === EnumSearchType.image || item.searchType === EnumSearchType.gif) {
         return (
-            <div onClick={onClick} key={safeCustomToString(item)} className={`w-48 h-48 p-1 rounded-lg bg-gray-3 flex flex-col justify-center items-center m-3  ${isSelectedItem ? "border-solid border-2 border-selected" : ""} hover:border-solid hover:border-2 hover:border-selected cursor-pointer`}>
+            <div ref={itemRef} onClick={onClick} key={safeCustomToString(item)} className={`w-48 h-48 p-1 rounded-lg bg-gray-3 flex flex-col justify-center items-center m-3  ${isSelectedItem ? "border-solid border-2 border-selected" : ""} hover:border-solid hover:border-2 hover:border-selected cursor-pointer`}>
                 <img src={item.memeUrl} alt={item.shortName} className="w-48 h-48 object-cover rounded-lg" />
                 <div className="text-textcolor text-center">
                     {item.shortName}
