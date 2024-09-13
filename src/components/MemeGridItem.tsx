56dbf14ee68a36751d516e2ac97167e8aae8face
Waleed <waleed.nizamaniwn@gmail.com>
2024-09-13 17:51:50 +0200

    added onclick to save for grid items

--- src/components/MemeGridItem.tsx
+++ src/components/MemeGridItem.tsx
@@ -1,6 +1,7 @@
 import { ModelEmoji } from "@/classes/ModelEmoji";
 import { EnumSearchType } from "@/enums";
 import { useMemeStore } from "@/hooks/useMemes";
+import { showErrorToast, showSuccessToast } from "@/utils/utils";
 
 interface GridItemProps {
     item: ModelEmoji;
@@ -9,7 +10,7 @@ interface GridItemProps {
 
 
 const GridItem: React.FC<GridItemProps> = ({ item, index }: GridItemProps) => {
-    const [currentSelectedGifIndex] = useMemeStore((state) => [state.currentSelectedGifIndex]);
+    const [currentSelectedGifIndex, currentSearchItems] = useMemeStore((state) => [state.currentSelectedGifIndex, state.currentSearchItems]);
 
     const safeCustomToString = (item: ModelEmoji) => {
         return item.customToString ? item.customToString() : `${item.shortName} - ${item.emoji} - ${item.searchType} - ${item.memeUrl}`;
@@ -22,21 +23,52 @@ const GridItem: React.FC<GridItemProps> = ({ item, index }: GridItemProps) => {
     // const isSelectedItem: boolean = currentSelectedGif !== null && currentSelectedGif instanceof  ModelEmoji && (item.customToString() === currentSelectedGif.customToString());
 
     const onClick = () => {
-        // copy to clipboard
-        // if (currentSelectedGifIndex >= 0 && currentSelectedGifIndex < currentSearchItems.length) {
-        //     const item = currentSearchItems[currentSelectedGifIndex];
-        //     // if (item.searchType)
-        //     navigator.clipboard.writeText(item.emoji);
-        //     // Show a toast
-        //     showSuccessToast("Copied to clipboard");
+        if (currentSelectedGifIndex >= 0 && currentSelectedGifIndex < currentSearchItems.length) {
+            const item = currentSearchItems[currentSelectedGifIndex];
+            if (item.searchType == EnumSearchType.ascii || item.searchType == EnumSearchType.emojis) {
+                console.log("Copying to clipboard: " + item.emoji);
+                navigator.clipboard.writeText(item.emoji);
+                // Show a toast
+                showSuccessToast("Copied to clipboard");
+            } else if (item.searchType == EnumSearchType.image) {
+                // Download the image and copy to clipboard
 
-        // }
+                fetch(item.memeUrl)
+                    .then(response => response.blob())
+                    .then(blob => {
+                        // Create a new clipboard item
+                        const data = [new ClipboardItem({ [blob.type]: blob })];
+
+                        // Copy the blob to the clipboard
+                        navigator.clipboard.write(data).then(() => {
+                            // Show a success toast
+                            showSuccessToast("Image/GIF copied to clipboard");
+                        }).catch(err => {
+                            // Handle errors
+                            showErrorToast("Failed to copy image/GIF to clipboard");
+                            console.error(err);
+                        });
+                    })
+                    .catch(err => {
+                        // Handle errors while fetching the image
+                        showErrorToast("Failed to download image/GIF");
+                        console.error(err);
+                    });
+
+            } else if (item.searchType == EnumSearchType.gif) {
+                // Download the image/Gif and copy to clipboard
+                console.log("Copying gif to clipboard: " + item.memeUrl);
+                navigator.clipboard.writeText(item.memeUrl);
+                // Show a toast
+                showSuccessToast("Copied to clipboard");
+            }
+        }
     }
 
     
     if (item.searchType === EnumSearchType.ascii || item.searchType === EnumSearchType.emojis) {
         return (
-            <div key={safeCustomToString(item)} className={`w-44 h-44 text-white rounded-lg bg-gray-3 flex flex-col justify-center items-center m-3 ${isSelectedItem ? "border-solid border-2 border-selected" : ""} hover:border-solid hover:border-2 hover:border-selected cursor-pointer`}>
+            <div onClick={onClick} key={safeCustomToString(item)} className={`w-44 h-44 text-white rounded-lg bg-gray-3 flex flex-col justify-center items-center m-3 ${isSelectedItem ? "border-solid border-2 border-selected" : ""} hover:border-solid hover:border-2 hover:border-selected cursor-pointer`}>
 
                 <div className={`mb-4 ${item.searchType === EnumSearchType.emojis ? "emoji-mart-emoji" : ""}`}>
                     {item.emoji}
@@ -50,7 +82,7 @@ const GridItem: React.FC<GridItemProps> = ({ item, index }: GridItemProps) => {
         );
     } else if (item.searchType === EnumSearchType.image || item.searchType === EnumSearchType.gif) {
         return (
-            <div key={safeCustomToString(item)} className={`w-48 h-48 p-1 rounded-lg bg-gray-3 flex flex-col justify-center items-center m-3  ${isSelectedItem ? "border-solid border-2 border-selected" : ""} hover:border-solid hover:border-2 hover:border-selected cursor-pointer`}>
+            <div onClick={onClick} key={safeCustomToString(item)} className={`w-48 h-48 p-1 rounded-lg bg-gray-3 flex flex-col justify-center items-center m-3  ${isSelectedItem ? "border-solid border-2 border-selected" : ""} hover:border-solid hover:border-2 hover:border-selected cursor-pointer`}>
                 <img src={item.memeUrl} alt={item.shortName} className="w-48 h-48 object-cover rounded-lg" />
                 <div className="text-textcolor text-center">
                     {item.shortName}
